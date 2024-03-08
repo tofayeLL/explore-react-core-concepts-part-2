9bf345e3c58493c3f6b71e9afb0f3f3cee2ba254
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-08 19:52:18 +0600

    learn and practice about event handler in react

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  function handleClick(){
+    alert('click me button');
+  }
+
+  const handleClick2 = () => {
+    alert('button 2 click');
+
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h3>Explore react core concepts part 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => { alert('third button clicked')}}>third </button>
+      <button onClick={() => addToFive(3) }>Four</button>
+    </>
+  )
+}
+
+export default App
